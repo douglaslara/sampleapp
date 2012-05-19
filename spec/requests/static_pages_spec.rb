2ecadd4facd6fa5df3755dfb18db72f6d6b76f13
Douglas Lara <douglas.lara@gmail.com>
2012-05-19 18:39:43 -0300

    Added a view helper class

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,14 @@ describe "Static pages" do
       page.should have_selector('h1', :text => 'Home')
     end
             
-    it "should have the title 'Home'" do
+    it "should not have the base title" do
       visit '/static_pages/home'
-      page.should have_selector('title', :text => 'Home')
+      page.should have_selector('title', :text => 'Ruby on Rails Tutorial Sample App')
+    end
+    
+    it "should not have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
     end
   end
   
